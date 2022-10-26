3857fe1dc7695474801b8f43b1c0502480f8add3
adamts028 <adamts028@gmail.com>
2022-10-27 03:13:29 +1300

    Added html and css and navagation between current pages

--- kiwinco/base/views.py
+++ kiwinco/base/views.py
@@ -1,3 +1,13 @@
+import os
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
+
+def home(request):
+    return render(request, 'base/home.html')
+
+def item(request):
+    return render(request, 'base/item.html')
+
+def catagory(request):
+    return render(request, 'base/catagory.html')
