b2a0c0e44c3aa3163cbb374a8b3b64fe99c351eb
lukeleech <lukeyoobee@gmail.com>
2022-11-03 14:07:48 +1300

    Login, logout, register functionality working and fits inside pop up, some visual bugs to work out still but core funcitonality is there

--- kiwinco/base/urls.py
+++ kiwinco/base/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
     path('<int:item_id>/', views.item, name='item'),
     path('<str:catagory>/', views.catagory, name='catagory'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
