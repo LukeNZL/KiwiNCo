a073a88ecc80828a7ba93496287858c2b7fc19c9
adamts028 <adamts028@gmail.com>
2022-11-21 23:10:45 +1300

    deployment attempts

--- kiwinco/settings.py
+++ kiwinco/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ad7-z@j96anv!lurxns8$y811fa)p@8ycd$l3_g*k$@&9_pz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [kiwinco-test-dev.ap-southeast-2.elasticbeanstalk.com]
 
 
 # Application definition
