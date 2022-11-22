61c468a3d3931c75dfd8cd28b52d09e241ab44ff
adamts028 <adamts028@gmail.com>
2022-11-22 20:04:10 +1300

    deployment

--- kiwinco/settings.py
+++ kiwinco/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'storages',
     'kiwinco',
 ]
 
@@ -167,6 +168,7 @@ STATICFILES_DIRS = [
    'static',
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATIC_ROOT = 'static'
 # Default primary key field type
